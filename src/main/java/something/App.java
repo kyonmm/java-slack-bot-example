cad980aed7a09b3553afaeaa5b4023a39c3259f7
kyonmm <kyon.mm@gmail.com>
2019-11-29 08:49:28 +0900

    add example bot

--- src/main/java/something/App.java
+++ src/main/java/something/App.java
@@ -3,12 +3,45 @@
  */
 package something;
 
-public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+import org.riversun.slacklet.Slacklet;
+import org.riversun.slacklet.SlackletRequest;
+import org.riversun.slacklet.SlackletResponse;
+import org.riversun.slacklet.SlackletService;
+import org.riversun.xternal.simpleslackapi.SlackChannel;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        String botToken ="xoxb-854564613383-853322617664-IqwPL7VwLOPF4NKzcTYfHlIB" ;
+
+        SlackletService slackService = new SlackletService(botToken);
+
+        // slackletを追加する
+        slackService.addSlacklet(new Slacklet() {
+
+            @Override
+            public void onMessagePosted(SlackletRequest req, SlackletResponse resp) {
+                // メッセージがユーザーからポストされた
+
+                // メッセージがポストされたチャンネルを取得する
+                SlackChannel channel = req.getChannel();
+
+                if ("example".equals(channel.getName())) {
+                    // #randomチャンネルだった場合
+
+                    // メッセージ本文を取得
+                    String content = req.getContent();
+
+                    // メッセージがポストされたチャンネルに対して、BOTからメッセージを送る
+                    resp.reply("「" + content + "」て言いましたね。");
+
+                }
+
+            }
+        });
+
+        // slackletserviceを開始(slackに接続)
+        slackService.start();
     }
 }
